6b74e7cb6f8aac86123ec35d200b9f6cf6665fdd
alexanderyanthar <alexander.yanthar@outlook.com>
2023-06-26 11:16:51 -0400

    started basic setup for currency tracker, check to see if api works as expected

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [chartData, setChartData] = useState([]);
+  const apiKey = 'GEDZHAFMQCTPRQZN';
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=EUR&to_symbol=USD&apikey=${apiKey}`);
+      const data = await response.json();
+      setChartData(data);
+    } catch(e) {
+    console.error('Error fetching data:', e);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Currency Exchange Rate Tracker</h1>
       </header>
+      <main className='App-main'>
+
+      </main>
+      <footer className='App=footer'>
+        <p>Powered by Alexander Yanthar</p>
+      </footer>
     </div>
   );
 }
